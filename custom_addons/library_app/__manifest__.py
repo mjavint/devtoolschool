3130c49baa6b7b2ecdd657b3bb037fbde430c974
mjavint <mjavint@gmail.com>
2023-07-18 12:03:55 -0400

    [ADD] Preparado para la clase ORM API - Comandos Especiales

--- custom_addons/library_app/__manifest__.py
+++ custom_addons/library_app/__manifest__.py
@@ -26,7 +26,6 @@
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
-        'views/sale_order_views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
