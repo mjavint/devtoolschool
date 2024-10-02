9af962604669cc7e9682c4173fd34cc74183962f
mjavint <mjavint@gmail.com>
2024-10-02 12:05:43 -0400

    [ADD] Add new version

--- owl_website/controllers/main.py
+++
@@ -1,8 +0,0 @@
-from odoo import http
-from odoo.http import request
-
-
-class Main(http.Controller):
-    @http.route('/owl/custom', type='http', auth='public', website=True)
-    def owl_custom(self, **kw):
-        return request.render('owl_website.custom_page')
