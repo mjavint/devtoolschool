864735204b1c394ba007e726c07845cda7d15194
Manuel Vinent Guilarte <mjavint@gmail.com>
2025-10-25 14:18:57 +0000

    [ADD] Se ha agregado el módulo de entrenamiento de pruebas

---
+++ training_test/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Training Test",
+    "version": "1.0.0",
+    "summary": """ Training to start implementing tests in Python and Javascript for Odoo """,
+    "author": "Odoo Brain",
+    "website": "https://www.youtube.com/@odoobrain",
+    "category": "Testing",
+    "depends": ["base", "web"],
+    "data": [],
+    "assets": {
+        "web.assets_backend": ["training_test/static/src/**/*"],
+    },
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
